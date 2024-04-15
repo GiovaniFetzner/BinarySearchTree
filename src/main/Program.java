1db3ad70bbb4f4b47ccb74734f9167feffddb627
Giovani <giovanifetzner01@gmail.com>
2024-04-15 17:27:39 -0300

    Implementacao de escrita base no arquivo .txt no formato Graphviz

--- src/main/Program.java
+++ src/main/Program.java
@@ -21,13 +21,35 @@ public class Program {
 		System.out.println("\n" + "PostOrdem:");
 		raiz.printPostOrder(raiz);
 		
+		System.out.println("Teste de escrita no arquivo");
+		printPreOrderSVG(raiz);
 	}
 	
 	private static void printPreOrderSVG(Nodo root) {
 		
-		String path = "C:\\Program Files\\Graphviz\\GraphvizArquivos";
+		String path = "C:\\Program Files\\Graphviz\\GraphvizArquivos\\BinarySearchTree.txt";
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+			writer.append("digraph ArvoreBinaria {");
+			writer.newLine();
+			writer.append("    nodesep = 0.5;");
+			writer.newLine();
+			writer.append("    node [shape=\"circle\", style=\"filled\", color=\"black\", fillcolor=\"#9370DB\"];");
+			writer.newLine();
+			writer.append("    edge [color=\"black\"];");
+			writer.newLine();
+			
+			
+			writer.append("A -> C");
+			writer.newLine();
+			writer.append("A -> B");
+			
+			writer.newLine();
+			writer.append("}");
+			writer.close();
+			
+			System.out.println("Escrita preOrder no .txt finalizada");
+			
 		} catch (IOException e) {
 			System.out.println("Erro no arquivo!");
 		}
