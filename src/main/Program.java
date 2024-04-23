51323775bd86dbb3afdb4fa4a5fa0e22a76baa59
Giovani <giovanifetzner01@gmail.com>
2024-04-23 14:59:12 -0300

    Geracao dos exemplos 02

--- src/main/Program.java
+++ src/main/Program.java
@@ -8,21 +8,19 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) {
-		Node raiz = new Node("A", 35);
+		Node raiz = new Node("A", 70);
 		
-		raiz.add("B", 47);
-		raiz.add("C", 53);
+		raiz.add("B", 82);
+		raiz.add("C", 15);
 		raiz.add("D", 48);
 		raiz.add("E", 28);
-		raiz.add("X", 30);
-		raiz.add("Y", 61);
+		raiz.add("X", 104);
+		raiz.add("Y", 22);
 		raiz.add("Z", 58);
 		raiz.add("W", 20);
-		raiz.add("U", 100);
-		raiz.add("V", 57);
+		raiz.add("U", 6);
+		raiz.add("V", 73);
 		
-		Node teste = new Node("teste", 60);
-		raiz.add(teste.getKey(), teste.getValue());
 		System.out.println("\n" + "PreOrdem:");
 		raiz.printPreOrder(raiz);
 		System.out.println("\n" + "InOrdem:");
@@ -30,26 +28,7 @@ public class Program {
 		System.out.println("\n" + "PostOrdem:");
 		raiz.printPostOrder(raiz);
 
-		System.out.println("\nTeste de retorno do StringBuilder:");
-		System.out.println(raiz.printGraphviz(raiz, new StringBuilder()));
-		
-
 		System.out.println("Pesquisas na arvore BST:");
-		
-		System.out.println("Busca valor 30 (existente): " + raiz.search(30));
-		System.out.println("Busca valor 31 (inexistente): " + raiz.search(31));
-		System.out.println();
-		System.out.println("Busca chave 'teste' (existente): " + raiz.search("teste"));
-		System.out.println("Busca chave 'brazil' (inexistente): " + raiz.search("brazil"));
-		System.out.println();
-		System.out.println("Menor valor da subarvore 60 (existente): " + raiz.searchMinValue(teste));
-		System.out.println("Menor valor da subarvore 200 (inexistente): " + raiz.searchMinValue(new Node("brazil", 200)));
-		System.out.println();
-		System.out.println("Maior valor da subarvore 47 (existente): " + raiz.searchMaxValue(new Node("B", 47)));
-		System.out.println("Maior valor da subarvore 200 (inexistente): " + raiz.searchMaxValue(new Node("brazil", 200)));
-		
-		raiz.deleteNode(58);
-		raiz.deleteNode(57);
 		
 		printSVG(raiz);
 		terminalProcess();
